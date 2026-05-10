802ab59b0f9eb6873f13e4dcb76705ea19804c5b
chzaeem47 <zaeemshahidak47@gmail.com>
2026-05-11 00:44:03 +0500

    Creating user account Schema

---
+++ src/models/account.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const accountSchema = new mongoose.Schema({
+
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref : 'user',
+        required : [true , "Account must be associated with a user!"],
+        index : true  // We can find users fastly
+    },
+
+    status: {
+        type : String,
+        uppercase : true,
+        enum:{
+            values : ["ACTIVE" , "FROZEN" , "CLOSED"],
+            message : "Status can be either Active,Frozen or Closed",
+        },
+        default : "ACTIVE"
+    },
+
+    currency:{
+        type : String,
+        required : true,
+        default : "PKR"
+    }
+},{
+    timestamps : true
+})
+
+accountSchema.index({ user:1 , status:1 })
+
+const accountModel = mongoose.model("account" , accountSchema)
+
+module.exports = {accountModel}
