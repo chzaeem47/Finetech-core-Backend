577cb69ab4de818460559136a52c3b33282d4910
chzaeem47 <zaeemshahidak47@gmail.com>
2026-05-12 17:45:56 +0500

    adding system user in user models

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
         type : Boolean,
         default : false,
         immutable : true,
-        select : false,
+        select : false
     },
 
     password:{
