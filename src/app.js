3959730ddfcfb76b58cb2642711855b7a7ad4bb9
chzaeem47 <zaeemshahidak47@gmail.com>
2026-05-16 00:02:10 +0500

    Restructring Folders

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
 const authRouter = require('../src/routes/auth.routes')
 const cookies = require('cookie-parser');
 const cookieParser = require('cookie-parser');
@@ -11,10 +10,6 @@ const app = express();
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
-    credentials: true
-}));
 
 /**
 * -- LOGIN,REGISTER ROUTER
